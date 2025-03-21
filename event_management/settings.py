7662c854ced7ddd50641c1b1b45b5b6a9e2abf07
Mahmud Hasan <mh.kid234@gmail.com>
2025-03-21 10:18:30 +0600

    h

--- event_management/settings.py
+++ event_management/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'https://127.0.0.1:8000/events']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000/events']
 
 
 # Application definition
